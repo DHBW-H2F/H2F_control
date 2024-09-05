0f7543612203056cb40f25c859d02c72f106e84e
E229815Z <E229815Z@etu.univ-nantes.fr>
2024-09-05 11:13:34 +0200

    adding 2 dashboard + configuration panel

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,45 @@
-const myModal = document.getElementById('myModal')
-const myInput = document.getElementById('myInput')
+document.addEventListener("DOMContentLoaded", function() {
+    // Fonction pour mettre à jour la valeur du slider
+    function updateValue(value) {
+        document.getElementById('sliderValue').textContent = value;
+    }
 
-myModal.addEventListener('shown.bs.modal', () => {
-  myInput.focus()
-})
+    // Événement de mise à jour du slider
+    var slider = document.getElementById('slider');
+    if (slider) {
+        slider.addEventListener('input', function() {
+            updateValue(this.value);
+        });
+    }
+
+     // Détecter le nom du fichier actuel dans l'URL
+     var currentPath = window.location.pathname;
+     var pageName = currentPath.substring(currentPath.lastIndexOf('/') + 1);
+ 
+     // Définir le texte par défaut du bouton
+     var buttonText = "Select Dashboard";
+ 
+     // Mettre à jour le texte du bouton en fonction de la page actuelle
+     if (pageName === "ProductionDashboard.html") {
+         buttonText = "Production Dashboard";
+     } else if (pageName === "LiveDashboard.html") {
+         buttonText = "Live Production Dashboard";
+     } else if (pageName === "ConfigurationPanel.html") {
+         buttonText = "Select a Dashboard";
+         document.body.classList.add('configuration-panel'); // Ajoute la classe pour masquer le bouton
+     }
+
+    // Mettre à jour le texte du bouton dropdown
+    var dropdownButton = document.getElementById("dropdownMenuButton");
+    if (dropdownButton) {
+        dropdownButton.textContent = buttonText;
+    }
+
+    // Script pour rafraîchir la page
+    var refreshButton = document.getElementById('refreshButton');
+    if (refreshButton) {
+        refreshButton.addEventListener('click', function() {
+            location.reload(); // Rafraîchir la page
+        });
+    }
+});
