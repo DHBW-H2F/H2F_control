8bd4754589c4bd9c22374ca7662c8e03a212b3ce
OctaveDaudin <octave.daudin.work@gmail.com>
2025-07-08 09:51:06 +0200

    update the config starting of implementing the separation of the logo

--- src/app_config.rs
+++ src/app_config.rs
@@ -1,7 +1,16 @@
 use serde::Deserialize;
-
+use s7_device::S7Device;
+use rocket::futures::lock::Mutex;
+use std::sync::Arc;
 #[derive(Deserialize, Debug)]
 pub struct AppConfig {
-    pub controller_ip: String,
-    pub controller_registers: String,
+    pub logo_ip: String,
+    pub logo_registers: String,
+
+    //  pub electrolyser_ip: String,
+    //  pub electrolyser_input_registers: String,
+    //  pub electrolyser_holding_registers: String,
+    
+    //  pub compressor_ip: String,
+    //  pub compressor_registers: String,
 }
