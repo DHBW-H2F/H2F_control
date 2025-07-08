ebf12e676208dfeb3c35d95e34b9646088c37346
OctaveDaudin <octave.daudin.work@gmail.com>
2025-07-08 09:50:28 +0200

    add the new route main factorisation of the sending command to the logo add the new register for the logo

--- src/control.rs
+++ src/control.rs
@@ -10,6 +10,21 @@ use custom_error::custom_error;
 
 custom_error! {PoisonedMutexError{err: String} = "Could not aquire lock ({err})"}
 
+pub async fn send_command_write(
+    device_m: &Arc<Mutex<impl IndustrialDevice>>, command: &str, value: &industrial_device::types::Value
+) -> Result<(),IndustrialDeviceError>{
+    let mut device = device_m.lock().await;
+    device.connect().await?;
+    device.write_register_by_name(command, value).await
+}
+
+pub async fn send_command_read(
+    device_m: &Arc<Mutex<impl IndustrialDevice>>, command: &str) -> Result<Value,IndustrialDeviceError>{
+    let mut device = device_m.lock().await;
+    device.connect().await?;
+    device.read_register_by_name(command).await
+}
+/*
 pub async fn start_electrolyzer(
     device_m: Arc<Mutex<impl IndustrialDevice>>,
 ) -> Result<(), IndustrialDeviceError> {
@@ -20,6 +35,7 @@ pub async fn start_electrolyzer(
         .write_register_by_name("Start/Stop", &Value::Boolean(true))
         .await
 }
+
 pub async fn stop_electrolyzer(
     device_m: Arc<Mutex<impl IndustrialDevice>>,
 ) -> Result<(), IndustrialDeviceError> {
@@ -54,3 +70,4 @@ pub async fn restart_electrolyzer(
         .write_register_by_name("Restart", &Value::Boolean(false))
         .await
 }
+*/
--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,36 @@
 #[macro_use]
 extern crate rocket;
-use core::panic;
-use std::fs::File;
-use std::sync::Arc;
-
-use rocket::futures::lock::Mutex;
-use rocket::http::Status;
-use rocket::response::status::Custom;
-use rocket::State;
-use s7_device::S7Device;
-
-use rocket::fs::FileServer;
-use rocket::response::Redirect;
-
-mod control;
 
 mod app_config;
 use app_config::AppConfig;
 
 use clap::Parser;
+mod control;
+use control::send_command_read;
+use control::send_command_write;
+use core::panic;
 
-struct AppState {
-    device: Arc<Mutex<S7Device>>,
-}
+use industrial_device::types::Value;
+
+
+use rocket::http::Status;
+use rocket::fs::FileServer;
+use rocket::futures::lock::Mutex;
+use rocket::response::Redirect;
+use rocket::response::status;
+use rocket::response::status::Custom;
+use rocket::State;
+
+use s7_device::S7Device;
+
+use serde::Serialize;
+
+use std::fs::File;
+use std::sync::Arc;
+
+
+
+
 
 // TODO: Add Reboot button
 
@@ -39,43 +47,204 @@ struct Args {
     config_file: String,
 }
 
-#[get("/")]
-async fn start(state: &State<AppState>) -> Result<(), Custom<String>> {
-    let res = control::start_electrolyzer(state.device.clone()).await;
+
+fn get_regs_file(file_path : String) -> File {
+    let regs_file: File = match File::open(&file_path) {
+        Ok(file) => file,
+        Err(err) => panic!(
+            "Could not open registers definition file : {err} ({0})",
+            file_path
+        ),
+    };
+    return regs_file;
+}
+
+fn create_s7_device(app_ip : String,regs_file : File) -> S7Device{
+    let device = S7Device::new(
+        match app_ip.parse() {
+            Ok(val) => val,
+            Err(err) => panic!(
+                "Could not parse controller adress : {err:?} ({0})",
+                app_ip
+            ),
+        },
+        match s7_device::utils::get_defs_from_json(regs_file) {
+            Ok(regs) => regs,
+            Err(err) => {
+                panic!("There was an error reading registers definition from file : {err:?}")
+            }
+        },
+    );
+    return device;
+}
+//--------------------------------------------------------------
+// route function
+
+
+use custom_error::custom_error;
+
+custom_error! {PoisonedMutexError{err: String} = "Could not aquire lock ({err})"}
+struct AppState {
+    logo: Arc<Mutex<S7Device>>,
+    //compressor: Arc<Mutex<S7Device>>,
+}
+#[derive(Serialize)]
+struct CompressorState {
+    state: String,
+}
+
+fn convert(valeur : Value) -> String{
+    match valeur {
+        Value::Enum16(val)=>val.to_string(),
+        Value::Float32(val)=>val.to_string(),
+        _ => "bad type".to_owned(),
+    }
+}
+
+#[get("/start")]
+async fn start_project(state: &State<AppState>) -> Result<(), Custom<String>> {
+    let res = send_command_write(&state.logo.clone(), "Start/Stop", &Value::Boolean(true)).await;
     match res {
         Ok(_) => Ok(()),
         Err(err) => Err(Custom(Status::InternalServerError, format!("{err:?}"))),
     }
 }
-#[get("/")]
-async fn stop(state: &State<AppState>) -> Result<(), Custom<String>> {
-    let res = control::stop_electrolyzer(state.device.clone()).await;
+#[get("/electrolyzer/start")]
+async fn start_electrolyzer(state: &State<AppState>) -> Result<(), Custom<String>> {
+    let res = send_command_write(&state.logo.clone(), "Start/Stop_electro", &Value::Boolean(true)).await;
     match res {
         Ok(_) => Ok(()),
         Err(err) => Err(Custom(Status::InternalServerError, format!("{err:?}"))),
     }
+
 }
-#[get("/?<rate>")]
-async fn prod_rate(rate: f32, state: &State<AppState>) -> Result<(), Custom<String>> {
-    let res = control::set_rate(state.device.clone(), rate).await;
-    match res {
-        Ok(_) => Ok(()),
-        Err(err) => Err(Custom(Status::InternalServerError, format!("{err:?}"))),
-    }
-}
-#[get("/")]
-async fn restart(state: &State<AppState>) -> Result<(), Custom<String>> {
-    let res = control::restart_electrolyzer(state.device.clone()).await;
+#[get("/compressor/start")]
+async fn start_compressor(state: &State<AppState>) -> Result<(), Custom<String>> {
+    let res = send_command_write(&state.logo.clone(), "Start/Stop_compressor", &Value::Boolean(true)).await;
     match res {
         Ok(_) => Ok(()),
         Err(err) => Err(Custom(Status::InternalServerError, format!("{err:?}"))),
     }
 }
 
+
+
+
+#[get("/stop")]
+async fn stop_project(state: &State<AppState>) -> Result<(), Custom<String>> {
+    let res = send_command_write(&state.logo.clone(), "Start/Stop", &Value::Boolean(false)).await;
+    match res {
+        Ok(_) => Ok(()),
+        Err(err) => Err(Custom(Status::InternalServerError, format!("{err:?}"))),
+    }
+}
+#[get("/electrolyzer/stop")]
+async fn stop_electrolyzer(state: &State<AppState>) -> Result<(), Custom<String>> {
+    let res = send_command_write(&state.logo.clone(), "Start/Stop_electro", &Value::Boolean(false)).await;
+    match res {
+        Ok(_) => Ok(()),
+        Err(err) => Err(Custom(Status::InternalServerError, format!("{err:?}"))),
+    }
+
+}
+#[get("/compressor/stop")]
+async fn stop_compressor(state: &State<AppState>) -> Result<(), Custom<String>> {
+    let res = send_command_write(&state.logo.clone(), "Start/Stop_compressor", &Value::Boolean(false)).await;
+    match res {
+        Ok(_) => Ok(()),
+        Err(err) => Err(Custom(Status::InternalServerError, format!("{err:?}"))),
+    }
+}
+
+#[get("/electrolyser/prodRate?<rate>")]
+async fn prod_rate(rate: f32, state: &State<AppState>) -> Result<(), Custom<String>> {
+    let res = send_command_write(&  state.logo.clone(),"Production_rate", &Value::Float32(rate)).await;
+    match res {
+        Ok(_) => Ok(()),
+        Err(err) => Err(Custom(Status::InternalServerError, format!("{err:?}"))),
+    }
+}
+
+
+#[get("/restart")]
+async fn restart_project(state: &State<AppState>) -> Result<(), Custom<String>> {
+    send_command_write(&state.logo.clone(),"Restart", &Value::Boolean(true)).await;
+    let res = send_command_write(&state.logo.clone(),"Restart", &Value::Boolean(false)).await;
+    match res {
+        Ok(_) => Ok(()),
+        Err(err) => Err(Custom(Status::InternalServerError, format!("{err:?}"))),
+    }
+}
+#[get("/electrolyzer/restart")]
+async fn restart_electrolyzer(state: &State<AppState>) -> Result<(), Custom<String>> {
+    send_command_write(&state.logo.clone(),"Restart_electro", &Value::Boolean(true)).await;
+    let res = send_command_write(&state.logo.clone(),"Restart_electro", &Value::Boolean(false)).await;
+    match res {
+        Ok(_) => Ok(()),
+        Err(err) => Err(Custom(Status::InternalServerError, format!("{err:?}"))),
+    }
+
+}
+#[get("/compressor/restart")]
+async fn restart_compressor(state: &State<AppState>) -> Result<(), Custom<String>> {
+    send_command_write(&state.logo.clone(),"Restart_compressor", &Value::Boolean(true)).await;
+    let res=send_command_write(&state.logo.clone(),"Restart_compressor", &Value::Boolean(false)).await;
+    match res {
+        Ok(_) => Ok(()),
+        Err(err) => Err(Custom(Status::InternalServerError, format!("{err:?}"))),
+    }
+}
+#[get("/compressor/state")]
+async fn get_state_compressor(state: &State<AppState>) -> status::Accepted<String> {
+    let res = send_command_read(&state.logo.clone(), "status_compr").await;
+    let result = match res {
+        Ok(value) => convert(value),
+        Err(_) => "none".to_owned(),
+    };
+    return status::Accepted(format!("state: '{}'", result));
+}
+#[get("/electrolyser/state")]
+async fn get_state_electrolyser(state: &State<AppState>) -> status::Accepted<String> {
+    let res = send_command_read(&state.logo.clone(),"status_electro").await;
+    let result = match res {
+        Ok(value) => convert(value),
+        Err(_) => "none".to_owned(),
+    };
+    return status::Accepted(format!("state: '{}'", result));
+}
+#[get("/electrolyser/prodValue")]
+async fn get_prod_value_compressor(state: &State<AppState>) -> status::Accepted<String> {
+    let res = send_command_read(&state.logo.clone(),"prodValue_electro").await;
+    let result = match res {
+        Ok(value) => convert(value),
+        Err(_) => "none".to_owned(),
+    };
+    return status::Accepted(format!("state: '{}'", result));
+}
+#[get("/compressor/prodValue")]
+async fn get_prod_value_electrolyser(state: &State<AppState>) -> status::Accepted<String> {
+    let res = send_command_read(&state.logo.clone(),"prodValue_compr").await;
+    let result = match res {
+        Ok(value) => convert(value),
+        Err(_) => "none".to_owned(),
+    };
+    return status::Accepted(format!("state: '{}'", result));
+}
+
 #[get("/")]
 fn index() -> Redirect {
     Redirect::to(uri!("/LiveDashboard.html"))
 }
+//--------------------------------------------------------------
+//--------------------------------------------------------------
+
+
+
+
+
+
+
+
 
 #[launch]
 fn rocket() -> _ {
@@ -88,38 +257,33 @@ fn rocket() -> _ {
 
     let app: AppConfig = config.try_deserialize().unwrap();
 
-    let regs_file: File = match File::open(&app.controller_registers) {
-        Ok(file) => file,
-        Err(err) => panic!(
-            "Could not open registers definition file : {err} ({0})",
-            app.controller_registers
-        ),
-    };
+    let regs_logo = get_regs_file(app.logo_registers);
+    let logo_device = create_s7_device(app.logo_ip, regs_logo);
 
-    let device = S7Device::new(
-        match app.controller_ip.parse() {
-            Ok(val) => val,
-            Err(err) => panic!(
-                "Could not parse controller adress : {err:?} ({0})",
-                app.controller_ip
-            ),
-        },
-        match s7_device::utils::get_defs_from_json(regs_file) {
-            Ok(regs) => regs,
-            Err(err) => {
-                panic!("There was an error reading registers definition from file : {err:?}")
-            }
-        },
-    );
-
+    //let regs_compressor: File = get_regs_file(app.compressor_registers);
+    //let compressor_device = create_s7_device(app.compressor_ip,regs_compressor);
     rocket::build()
         .mount("/", routes![index])
         .mount("/", FileServer::from("./static"))
-        .mount("/start", routes![start])
-        .mount("/stop", routes![stop])
-        .mount("/prodRate", routes![prod_rate])
-        .mount("/restart", routes![restart])
+        .mount("/start", routes![start_project])
+        .mount("/stop", routes![stop_project])
+        .mount("/restart", routes![restart_project])
+
+        .mount("/compressor/start", routes![start_compressor])
+        .mount("/compressor/stop", routes![stop_compressor])
+        .mount("/compressor/restart", routes![restart_compressor])
+        .mount("/compressor/getState", routes![get_state_compressor])
+        .mount("/compressor/getProdValue", routes![get_prod_value_compressor])
+
+        .mount("/electrolyser/start", routes![start_electrolyzer])
+        .mount("/electrolyser/stop", routes![stop_electrolyzer])
+        .mount("/electrolyser/restart", routes![restart_electrolyzer])
+        .mount("/electrolyser/prodRate", routes![prod_rate])
+        .mount("/electrolyser/getState", routes![get_state_electrolyser])
+        .mount("/electrolyser/getProdValue", routes![get_prod_value_electrolyser])
+
         .manage(AppState {
-            device: Arc::new(Mutex::new(device)),
+                logo: Arc::new(Mutex::new(logo_device)),
+                //compressor: Arc::new(Mutex::new(compressor_device)),
         })
 }
