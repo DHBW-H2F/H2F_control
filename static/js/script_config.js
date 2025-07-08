9c234210ed4de54e035a6bf0c77177361ba99ff5
OctaveDaudin <octave.daudin.work@gmail.com>
2025-07-08 09:49:29 +0200

    add and update the js function for the new window

--- static/js/script_config.js
+++ static/js/script_config.js
@@ -1,28 +1,50 @@
 document.addEventListener("DOMContentLoaded", function() {
     // start/stop action
-    const startButton = document.getElementById('startButton');
-    const stopButton = document.getElementById('stopButton');
-    const restartButton = document.getElementById('restartButton');
+    const startButtons = document.querySelectorAll('.startButton');
+    const stopButtons = document.querySelectorAll('.stopButton');
+    const restartButtons = document.querySelectorAll('.restartButton');
+    const statusEle = document.querySelectorAll(".sys_status");
+    const prod_valuesEle= document.querySelectorAll(".sys_prod_value");
+    const switchbuttons = document.getElementById("switch-screen").querySelectorAll("button");
+    const screens=document.getElementById("main-container").querySelectorAll(".sys_screen");
 
-    startButton.addEventListener('click', async function() {
-        const response = await fetch("/start");
+    startButtons.forEach((button)=>{
+        button.addEventListener('click', async function() {
+            let device = button.id.split("_")[1];
+            if(device!==undefined) device+="/";
+            const response = await fetch("/"+device+"start");
+        });
     });
-    stopButton.addEventListener('click', async function() {
-        const response = await fetch("/stop");
+    stopButtons.forEach((button)=>{
+        button.addEventListener('click', async function() {
+            let device = button.id.split("_")[1];
+            if(device!==undefined) device+="/";
+            const response = await fetch("/"+device+"stop");
+        });
     });
-    restartButton.addEventListener('click', async function() {
-        const response = await fetch("/restart");
+    restartButtons.forEach((button)=>{
+        button.addEventListener('click', async function() {
+            let device = button.id.split("_")[1];
+            if(device!==undefined) device+="/";
+            const response = await fetch("/"+device+"restart");
+        });
     });
-
-
     // Production rate
     const prodRate = document.getElementById('slider');
 
     prodRate.addEventListener("change", async function () {
-        const request = await fetch("/prodRate?" + new URLSearchParams({
+        const request = await fetch("/electrolyser/prodRate?" + new URLSearchParams({
             rate: prodRate.value,
         }));
-    })
+    });
+    switchbuttons.forEach(btn=>{
+        btn.addEventListener("click",(evt)=>{
+            switchbuttons.forEach((b)=>b.classList.remove("disabled"));
+            screens.forEach((screen)=>{screen.classList.add("d-none")});
+            evt.target.classList.add("disabled");
+            document.getElementById(evt.target.value).classList.remove("d-none")
+        })
+    });
 
     // Fonction pour mettre à jour la valeur du slider
     function updateValue(value) {
@@ -34,5 +56,33 @@ document.addEventListener("DOMContentLoaded", function() {
     slider.addEventListener('input', function() {
         updateValue(this.value);
     });
+
+    function getRandomValue(){
+        let a = Math.random()*100;
+        return (a)-(a%1);
+    }
+    
+    function updateProdValue(){
+        statusEle.forEach(async (stat)=>{
+            let device = stat.id.split("_")[1];
+            if(device!==undefined) device+="/";
+            const response = await fetch("/"+device+"getState");
+        });
+        prod_valuesEle.forEach(async (prod_value)=>{
+            let device = stat.id.split("_")[1];
+            if(device!==undefined) device+="/";
+            const response = await fetch("/"+device+"getProdValue");
+        });
+    }
+
+    async function  updateStateAndValue(){
+        while(true){
+            const delay = ()=>{return new Promise(resolve => setTimeout(resolve, 5000))}
+            await delay().then(()=>{
+                updateProdValue();
+            });
+        }
+    }
+    updateStateAndValue();
 
 });
